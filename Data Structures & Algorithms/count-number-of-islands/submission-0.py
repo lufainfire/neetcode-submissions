173972d3721ed1f836de12efb46bf8328435f026
Fadi Rasmy <161151625+lufainfire@users.noreply.github.com>
2026-09-15 01:01:34 -0400

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def explore(i, j):
+            grid[i][j]="0"
+            if i<len(grid)-1 and grid[i+1][j]=="1": #up
+                explore(i+1,j)
+            if i>0 and grid[i-1][j]=="1": #down
+                explore(i-1,j)
+            if j<len(grid[0])-1 and grid[i][j+1]=="1":
+                explore(i,j+1)
+            if j>0 and grid[i][j-1]=="1":
+                explore(i,j-1)
+        
+        answer=0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]=="1":
+                    answer+=1
+                    print(i,j)
+                    explore(i,j)
+        return answer
+        
